6aff14b3e1f97a7de3453feb9313d90aac9a85fa
Serhii <S_Pavlichuk@icloud.com>
2022-05-02 23:59:58 +0300

    add CustomColors add BlankGameViewController

---
+++ Alias/Controllers/GameViewController.swift
@@ -0,0 +1,17 @@
+//
+//  GameViewController.swift
+//  Alias
+//
+//  Created by admin on 02.05.2022.
+//
+
+import Foundation
+import UIKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
