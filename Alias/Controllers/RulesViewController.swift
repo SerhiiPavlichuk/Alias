25f9627fce4e726bfa2033e9f2bca7c8ce41d173
mazurich <avo.mazurich@gmail.com>
2022-05-03 19:01:31 +0300

    Added text with the rules of the game on Rules screen

--- Alias/Controllers/RulesViewController.swift
+++ Alias/Controllers/RulesViewController.swift
@@ -8,9 +8,19 @@
 import UIKit
 
 class RulesViewController: UIViewController {
-
+    
+    @IBOutlet weak var RulesLabel: UILabel!
+    let rules = Rules()
+    
+    
+    
     override func viewDidLoad() {
+        RulesLabel.numberOfLines = 0
+        RulesLabel.lineBreakMode = .byWordWrapping
+        
+        RulesLabel.text = rules.rules
+        
         super.viewDidLoad()
-
     }
+    
 }
