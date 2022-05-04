6a4d74e54bf5a32ec3f29c10f61d775bd42cf3d2
mazurich <avo.mazurich@gmail.com>
2022-05-04 21:48:48 +0300

    completed the joke request code

---
+++ Alias/Model/JokeData.swift
@@ -0,0 +1,13 @@
+//
+//  JokeData.swift
+//  Alias
+//
+//  Created by Oleg Mazuro on 4.05.22.
+//
+
+import Foundation
+
+struct JokeData : Decodable {
+    let setup: String
+    let punchline: String
+}
--- Alias/Model/JokeManager.swift
+++ Alias/Model/JokeManager.swift
@@ -6,24 +6,41 @@
 //
 
 import Foundation
+
 struct JokeManager {
     
+    func fetchJoke() {
+        let urlString = "https://joke.deno.dev/" // нужна ли эта функция? может лучше адрес сайта вставить в функцию performRequest?
+        performRequest(urlString: urlString)
+    }
+    
     func performRequest(urlString: String) {
-        if let url = URL(string: "https://joke.deno.dev/") {
+        if let url = URL(string: urlString) {
             let session = URLSession(configuration: .default)
-            let task = session.dataTask(with: url, completionHandler: handle(data:response:error:))
+            let task = session.dataTask(with: url) { data, response, error in
+                if error != nil {
+                    print (error!)
+                    return
+                }
+                if let safeData = data {
+                    let dataString = String(data: safeData, encoding: .utf8)
+                }
+            }
             task.resume()
         }
     }
     
-    func handle(data: Data?, response: URLResponse?, error: Error?) {
-        if error != nil {
-            print (error!)
-            return
-        }
-        if let safeData = data {
-            let dataString = String(data: safeData, encoding: .utf8)
-            print(dataString)
+    func parseJSON(jokeData: Data) {
+        let decoder = JSONDecoder()
+        do {
+            let decoderData = try decoder.decode(JokeData.self, from: jokeData)
+            let firstStringJoke = decoderData.setup
+            let secondStringJoke = decoderData.punchline
+            
+            print(firstStringJoke) //поверка вывода первой строки шутки в консоль
+            print(secondStringJoke) //поверка вывода второй строки шутки в консоль
+        } catch {
+            print(error)
         }
     }
 }
