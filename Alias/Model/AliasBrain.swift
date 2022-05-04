143c63c595ea07ba02bb28c5a252c950c5c48d08
Artem Borodin <parapro.kh@gmail.com>
2022-05-04 18:01:29 +0200

    add AliasBrain struct

---
+++ Alias/Model/AliasBrain.swift
@@ -0,0 +1,19 @@
+//
+//  AliasBrain.swift
+//  Alias
+//
+//  Created by Артем Бородин on 04.05.2022.
+//
+
+import Foundation
+
+struct AliasBrain {
+    
+    let actors = ["Chris Evans", "Robert Downey, Jr.", "Jennifer Lawrence", "Channing Tatum", "George Clooney", "Johnny Depp", "Margot Robbie", "Jared Leto", "Robert De Niro", "Jack Nicholson", "Marlon Brando", "Denzel Washington", "Meryl Streep", "Tom Hanks", "Leonardo DiCaprio", "Morgan Freeman", "Charles Chaplin", "Al Pacino", "Halle Berry", "James Dean"]
+    
+    let countries = ["China", "Czech Republic", "Egypt", "Finland", "France", "Honduras", "Japan", "Malawi", "Nepal", "Poland", "Spain", "Tajikistan", "Ukraine", "Yemen", "Zimbabwe", "South Africa", "Senegal", "Rwanda", "Qatar", "Niger"]
+    
+    let biggestCities = ["Tokyo", "Jakarta", "Delhi", "Manila", "Seoul", "Shanghai", "Karachi", "Beijing", "New York", "Guangzhou", "Sao Paulo", "Mexico City", "Mumbai", "Osaka", "Dhaka", "Cairo", "Los Angeles", "Bangkok", "Kolkata", "Buenos Aires"]
+    
+    let companies = ["Apple", "Saudi Aramco", "Microsoft", "Google", "Amazon", "Tesla", "Berkshire Hathaway", "Facebook", "Johnson & Johnson", "NVIDIA", "Visa", "Mastercard", "Bank of America", "Coca-Cola", "Toyota", "Walt Disney", "Oracle", "Nike", "Intel", "Salesforce"]
+}
