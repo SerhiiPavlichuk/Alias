253a6e04cfab0aad546fc080e83a36dfb9ad826a
Serhii <S_Pavlichuk@icloud.com>
2022-05-05 09:27:24 +0300

    update jokePresent

--- Alias/Model/JokePresent.swift
+++ Alias/Model/JokePresent.swift
@@ -8,8 +8,8 @@
 import UIKit
 
 struct JokePresent {
-    func showJokeMessage(from text: String, to viewController: UIViewController) {
-        let alertController = UIAlertController(title: "Ask the question:", message: text,  preferredStyle: .alert)
+    func showJokeMessage(from question: String, to joke: String, to viewController: UIViewController) {
+        let alertController = UIAlertController(title: question, message: joke,  preferredStyle: .alert)
         let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
         alertController.addAction(okAction)
         viewController.present(alertController, animated: true, completion: nil)
